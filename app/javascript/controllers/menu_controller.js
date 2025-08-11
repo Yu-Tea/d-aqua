1f98f4ee257d5d4f1afce881f7d7f9deec921059
Yu-Tea <y68tanaka@gmail.com>
2025-08-11 14:56:05 +0900

    メニュー画面簡易作成

---
+++ app/javascript/controllers/menu_controller.js
@@ -0,0 +1,18 @@
+import { Controller } from "@hotwired/stimulus";
+
+// Connects to data-controller="menu"
+export default class extends Controller {
+  static targets = ["panel"];
+
+  toggle() {
+    this.panelTarget.classList.toggle("active");
+  }
+
+  close() {
+    this.panelTarget.classList.remove("active");
+  }
+
+  stop(event) {
+    event.stopPropagation();
+  }
+}
