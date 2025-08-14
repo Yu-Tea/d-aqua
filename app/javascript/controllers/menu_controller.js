3779a6aeed145617e9eed011945cb2276c06ee85
Yu-Tea <y68tanaka@gmail.com>
2025-08-14 15:54:45 +0900

    ログインアウトにフラッシュメッセージ調整

--- app/javascript/controllers/menu_controller.js
+++ app/javascript/controllers/menu_controller.js
@@ -13,6 +13,14 @@ export default class extends Controller {
   }
 
   stop(event) {
+    // ログアウトリンクの場合は特別処理
+    const isLogout = event.target.closest('[data-turbo-method="delete"]');
+
+    if (isLogout) {
+      this.close(); // メニューを閉じる
+      return; // イベント処理を通す
+    }
+
     event.stopPropagation();
   }
 }
