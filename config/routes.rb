cc4ea1eb611f04cf2f134c639b5da1079f81599f
Yu-Tea <y68tanaka@gmail.com>
2025-08-17 21:07:08 +0900

    プロフィール等各種ページ追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,13 @@ Rails.application.routes.draw do
   resources :users, only: %i[new create]
   resources :creatures, only: %i[index new create show destroy]
   resources :books, only: %i[index]
+  resource :profile, only: %i[show edit update]
 
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
+
+  get '/how_to_play',   to: 'static_pages#how_to_play',   as: :how_to_play
+  get '/terms',   to: 'static_pages#terms',   as: :terms
+  get '/privacy', to: 'static_pages#privacy', as: :privacy
 end
