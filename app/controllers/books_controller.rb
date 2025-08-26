2e804a84fc665819b1426e7f3c5e1d886962f0bc
Yu-Tea <y68tanaka@gmail.com>
2025-08-26 15:18:34 +0900

    遊び方ページ完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,7 @@ class BooksController < ApplicationController
   before_action :require_login
 
   def index
-    @books = current_user.books.includes(creature: :user).order(created_at: :desc).page(params[:page]).per(10)
+    @books = current_user.books.includes(creature: :user).order(created_at: :desc).page(params[:page])
 
     @discovered_count = current_user.books.count
     @total_creatures_count = Creature.count
