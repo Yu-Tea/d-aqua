79f73193e00f2bad13b34294c613f213ba3630f5
Yu-Tea <y68tanaka@gmail.com>
2025-08-28 20:10:42 +0900

    🦈fix:OGP画像生成用の調整

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,8 +103,4 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-  # タイムアウト時間を延長
-  config.timeout = 60
-  # プロセス数を減らしてメモリ確保
-  WEB_CONCURRENCY=1  
 end
